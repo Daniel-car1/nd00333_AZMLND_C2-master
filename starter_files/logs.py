fad5195010bddae483a5279e5293e6bd1c78462f
Daniel-car1 <76181447+Daniel-car1@users.noreply.github.com>
2021-01-18 20:49:11 +0100

    Update logs.py
    added application insights

--- starter_files/logs.py
+++ starter_files/logs.py
@@ -4,11 +4,14 @@ from azureml.core.webservice import Webservice
 # Requires the config to be downloaded first to the current working directory
 ws = Workspace.from_config()
 
-# Set with the deployment name
+# Set with the deployment name 
 name = ""
 
 # load existing web service
 service = Webservice(name=name, workspace=ws)
+
+service.update(enable_app_insights=True)
+
 logs = service.get_logs()
 
 for line in logs.split('\n'):
