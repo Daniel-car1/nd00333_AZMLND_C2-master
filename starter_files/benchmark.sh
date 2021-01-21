49b2a3a69a19064f8eb01f87f9011292a87dde6a
Daniel-car1 <76181447+Daniel-car1@users.noreply.github.com>
2021-01-21 19:29:53 +0000

    Add files via upload
    First run of project 2

--- starter_files/benchmark.sh
+++ starter_files/benchmark.sh
@@ -70,4 +70,4 @@
 # Total:        114  160  28.0    172     199#
 
 
- ab -n 10 -v 4 -p data.json -T 'application/json' -H 'Authorization: Bearer REPLACE_WITH_KEY' http://REPLACE_WITH_API_URL/score
+ ab -n 10 -v 4 -p data.json -T 'application/json' -H 'Authorization: Bearer giBwUdwAaT3ZrYuiy4oXSEaqfOJMgMTj' http://86000479-1e15-442f-adda-e09884911f78.southcentralus.azurecontainer.io/score
--- starter_files/logs.py
+++ starter_files/logs.py
@@ -4,12 +4,13 @@ from azureml.core.webservice import Webservice
 # Requires the config to be downloaded first to the current working directory
 ws = Workspace.from_config()
 
-# Set with the deployment name 
-name = ""
+# Set with the deployment name
+name = "automl-bank-deploy"
 
 # load existing web service
 service = Webservice(name=name, workspace=ws)
 
+# enable application insight
 service.update(enable_app_insights=True)
 
 logs = service.get_logs()
